e6bffc368322e945241699fbb7ca016275dfa58a
reedhook <reedhooks0@gmail.com>
2023-10-15 14:28:45 +0700

    fix ALbumService

--- app/Services/AlbumService.php
+++ app/Services/AlbumService.php
@@ -9,24 +9,27 @@ use Illuminate\Support\Facades\Storage;
 
 class AlbumService
 {
-    public function store($response){
-        foreach($response as $item){
+    public function store($response)
+    {
+        foreach ($response as $item) {
             try {
                 DB::beginTransaction();
-                $album=[
-                    'id'=>$item['id'],
-                    'name'=>$item['name'],
-                    'description'=>$item['description']??null,
-                    'count_tracks'=>$item['total_count'],
-                    'cover'=>isset($item['cover'])?$item['id'].'.'.pathinfo($item['cover']): null,
-                    'minutes'=>$item['total_duration']['minutes']??null,
-                    'seconds'=>$item['total_duration']['seconds']??null,
-                    'milliseconds'=>$item['total_duration']['milliseconds']??null,
+                $album = [
+                    'id' => $item['id'],
+                    'name' => $item['name'],
+                    'description' => $item['description'] ?? null,
+                    'count_tracks' => $item['total_count'],
+                    'cover' => isset($item['cover']) ? $item['id'] . '.jpg' : null,
+                    'minutes' => $item['total_duration']['minutes'] ?? null,
+                    'seconds' => $item['total_duration']['seconds'] ?? null,
+                    'milliseconds' => $item['total_duration']['milliseconds'] ?? null,
                 ];
                 Album::firstOrCreate($album);
                 DB::commit();
-                Storage::disk('public')->put('/images/' . $album['cover'], file_get_contents
-                ($item['cover']));
+                if (isset($item['cover'])) {
+                    Storage::disk('public')->put('/images/' . $album['cover'], file_get_contents
+                    ($item['cover']));
+                }
             } catch (Exception $exeption) {
                 DB::rollBack();
                 abort(500);
